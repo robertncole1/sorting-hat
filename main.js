e91d8e73e035093d6500dde701c619476ce6e9cd
Rob Cole <robertncole1@gmail.com>
2021-01-30 11:25:21 -0600

    added in array of objects

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 console.log('Hello World');
+
+const students = [
+  {
+    name: "Harry Potter",
+    house: "Gryffindor",
+    id: "HP",
+  },
+  {
+    name: "Hermione Granger",
+    house: "Gryffindor",
+    id: "HG",
+  },
+  {
+    name: "Ron Weasley",
+    house: "Gryffindor",
+    id: "RW",
+  },
+  {
+    name: "Draco Malfoy",
+    house: "Slytherin",
+    id: "DM",
+  },
+];
