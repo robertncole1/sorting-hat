6c53df26547dd6f8fade4de6a7f98ca76855f8fc
Rob Cole <robertncole1@gmail.com>
2021-02-01 20:44:27 -0600

    Added in delete functionality but not printing new card right

--- main.js
+++ main.js
@@ -1,35 +1,70 @@
-console.log('Hello World');
+console.log("Hello World");
 
-const students = []
+const students = [];
+let expelledStudents = [];
 
 const hogwartsHouse = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
-
 
 const printToDom = (divId, textToPrint) => {
   const selectedDiv = document.querySelector(divId);
   selectedDiv.innerHTML = textToPrint;
-}
+};
 
 const formBuilder = () => {
-    document.querySelector("#new-student-form").style.display = 'block';
-}
+  document.querySelector("#new-student-form").style.display = "block";
+};
 
 const getFormInfo = (e) => {
   e.preventDefault();
-    const name = document.querySelector('#name').value;
+  const name = document.querySelector("#name").value;
 
+  const house = hogwartsHouse[Math.floor(Math.random() * hogwartsHouse.length)];
 
-    const obj = {
-      name,
-      house: hogwartsHouse[Math.floor(Math.random() * hogwartsHouse.length)],
-    }
- 
+  // get all the student IDs and create a new array and sort them lowest to highest
+  const studentIds = students
+    .map((student) => student.id)
+    .sort((a, b) => a - b);
+
+  // ternary operator: if the students array is not empty, create an ID that is +1 of the last item in the array. Otherwise make the id 1
+  const id = studentIds.length ? studentIds[studentIds.length - 1] + 1 : 1;
+
+  const obj = {
+    name,
+    house,
+    id,
+  };
+
   students.push(obj);
   studentBuilder(students);
-  document.querySelector('form').reset();
-}
+  document.querySelector("form").reset();
+};
 
 const studentBuilder = (taco) => {
+  let dom = "";
+  for (const [i, item] of taco.entries()) {
+    dom += `<div class="card my-2" style="width: 18rem;" id=${i}>
+    <div class="card-body">
+      <h5 class="card-title">${item.name}</h5>
+      <p class="card-text">${item.house}</p>
+      <button type="button" class="btn btn-danger" id="${i}">Expel</button>
+    </div>
+  </div>`;
+  }
+  printToDom("#new-student", dom);
+};
+
+const deleteStudent = (e) => {
+  const targetType = e.target.type;
+  const targetId = e.target.id;
+  if (targetType === "button") {
+    let expelled = students.splice(targetId, 1);
+    expelledStudents.push(expelled);
+  }
+  studentBuilder(students);
+  expelledBuilder(expelledStudents);
+};
+
+const expelledBuilder = (taco) => {
   let dom = "";
   for (const [i, item] of taco.entries()) {
     dom += `<div class="card my-2" style="width: 18rem;" id=${i}>
@@ -39,18 +74,19 @@ const studentBuilder = (taco) => {
     </div>
   </div>`;
   }
-  printToDom('#new-student', dom);
-}
-
+  printToDom("#expel-student", dom);
+};
 
 const btnEvents = () => {
-  document.querySelector('#Sorting').addEventListener('click', formBuilder);
-  document.querySelector('form').addEventListener('submit', getFormInfo);
-}
+  document.querySelector("#Sorting").addEventListener("click", formBuilder);
+  document.querySelector("form").addEventListener("submit", getFormInfo);
+  document.querySelector("#new-student").addEventListener("click", deleteStudent);
+};
 
 const init = () => {
   console.log(students);
+  console.log(expelledStudents);
   btnEvents();
-}
+};
 
 init();
